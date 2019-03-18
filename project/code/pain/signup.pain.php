4995c57c5f36d407219bdad87b509f5ff714d718
Nithin Amaraboina <48430674+nithinpain@users.noreply.github.com>
2019-03-18 05:44:38 -0700

    Add files via upload

---
+++ project/code/pain/signup.pain.php
@@ -0,0 +1,57 @@
+<?php
+if (isset($_POST['signup-submit']))
+ {
+    require "db.pain.php";
+  
+    $username=$_POST['uid'];
+    $email=$_POST['mail'];
+    $password=$_POST['pwd'];
+    $passwordRepeat=$_POST['pwd-repeat'];
+    if (empty($username)|| empty($email)|| empty($password)|| empty($passwordRepeat)) {}
+    else if ($password !== $passwordRepeat){
+        header("location: ../registration.php?");
+        exit();
+     }
+    else{
+          $sql = "SELECT uidUsers FROM users WHERE uidUsers=?";
+           $stmt=mysqli_stmt_init($conn);  
+          if (!mysqli_stmt_prepare($stmt,$sql)){
+            header("location: ../registration.php?error=sqlerror");
+               exit();
+               }
+            else
+               {
+             mysqli_stmt_bind_param($stmt,"s",$username);
+             mysqli_stmt_execute($stmt);
+              mysqli_stmt_store_result($stmt);}
+                $resultCheck=mysqli_stmt_num_rows($stmt);}
+               if ($resultCheck>0)
+                {
+             
+              header("location: ../registration.php?error=username&taken".$username);
+              
+            
+              exit();
+      
+             }
+              else {
+               $sql="INSERT INTO users(uidUsers,emailUsers,pwdUsers) VALUES (?,?,?)";
+             $stmt=mysqli_stmt_init($conn);}
+             if (!mysqli_stmt_prepare($stmt,$sql)) {
+               header("location: ../registration.php?error=sqlerror");
+              exit();
+             } 
+              else {
+                  $hashedpwd=password_hash($password,PASSWORD_DEFAULT);
+
+
+             mysqli_stmt_bind_param($stmt,"sss",$username,$email,$hashedpwd);
+             mysqli_stmt_execute($stmt);
+             header("location: ../signup.html?signup=sucess");
+             exit();
+            }
+        }
+        mysqli_stmt_close($stmt);
+        mysqli_close($conn);
+      
+
---
+++ project/code/upload.php
@@ -0,0 +1,38 @@
+<?php
+
+
+
+
+if (isset($_POST['submit'])) {
+    $file=$_FILES['file'];
+
+    $fileName=$_FILES['file']['name'];
+    $fileTmpName=$_FILES['file']['tmp_name'];
+    $fileSize=$_FILES['file']['size'];
+    $fileError=$_FILES['file']['error'];
+    $fileType=$_FILES['file']['type'];
+
+    $fileExt=explode('.',$fileName);
+    $fileActualExt=strtolower(end($fileExt));
+
+    $allowed=array('jpg','jpeg');
+
+    if (in_array($fileActualExt,$allowed)) {
+        if ($fileError==0) {
+            if ($fileSize<1000000) {
+               $fileNameNew="mypost".'.'.$fileActualExt;
+               $fileDestination='uploads/'.$fileNameNew;
+               move_uploaded_file($fileTmpName,$fileDestination);
+           
+               header("location: copyhome.html?uploadsuccess");
+            }else {
+                
+                header("location: post.php?uploadfailed");
+            }
+        }else {
+            echo "uploading error";
+        }
+    }else {
+        echo "check file type & upload again";
+    }
+}?>
